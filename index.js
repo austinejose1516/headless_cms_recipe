c8e234843bbdd27e6987e9c728fe9d9d20eab113
Austine Jose <austinejose1516@gmail.com>
2021-02-27 23:06:02 +0000

    1.1

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+// Fetching data from Contentful using Space ID and Access Token
 const client = contentful.createClient({
     space: 'c3yfente4cah',
     accessToken: '4pHtlonQbFIAAP4Hhi5d8kNIiYwU8rLtYcrqSBAuqz8',
 });
 
+// Calling API
 var markup = '';
-
 client.getEntries().then(function (entries) {
-    // log the title for all the entries that have it
     entries.items.forEach(function (entry) {
         if (entry.fields.name) {
             markup +=
                 "<div class='flip-card'><div class='flip-card-inner'><div class='flip-card-front'><img src='https://" +
                 entry.fields.featuredImage.fields.file.url +
-                "'/><h1 class='title'>" +
+                "'/><h1 class='heading-primary__card-title'>" +
                 entry.fields.name +
-                "</h1><hr /></div><div class='flip-card-back'><p>" +
+                "</h1><hr /></div><div class='flip-card-back'><p class='heading-primary__card-body'>" +
                 entry.fields.description +
                 '</p></div></div></div>';
         }
